61f66c377f9e11d7725e9239b1a1f2a709002f7c
Konstantinos Panayiotou <klpanagi@gmail.com>
2023-01-08 20:30:53 +0200

    Implements endpoint to generate graph images from models

--- smauto_server/smauto_server.py
+++ smauto_server/smauto_server.py
@@ -5,10 +5,15 @@ import os
 import base64
 from typing import Optional
 import subprocess
+import shutil
 
 import tarfile
 
 from smauto.language import build_model
+from smauto.generator import (
+    generate_automation_graph,
+    pu_to_png_transformation
+)
 
 from fastapi import FastAPI, File, UploadFile, status
 from fastapi.responses import HTMLResponse, FileResponse, JSONResponse
@@ -140,6 +145,63 @@ async def interpret(model_file: UploadFile = File(...),
     return resp
 
 
+@api.post("/graph")
+async def generate(model_file: UploadFile = File(...)):
+    print(f'Generate for request: file=<{model_file.filename}>,' + \
+          f' descriptor=<{model_file.file}>')
+    resp = {
+        'status': 200,
+        'message': ''
+    }
+    fd = model_file.file
+    u_id = uuid.uuid4().hex[0:8]
+    model_path = os.path.join(
+        TMP_DIR,
+        f'model-{u_id}.smauto'
+    )
+    tarball_path = os.path.join(
+        TMP_DIR,
+        f'graph-{u_id}.tar.gz'
+    )
+    gen_path = os.path.join(
+        TMP_DIR,
+        f'gen-{u_id}'
+    )
+    if not os.path.exists(gen_path):
+        os.mkdir(gen_path)
+    with open(model_path, 'w') as f:
+        f.write(fd.read().decode('utf8'))
+    try:
+        model = build_model(model_path)
+        # Build entities dictionary in model. Needed for evaluating conditions
+        model.entities_dict = {entity.name: entity for entity in model.entities}
+        for automation in model.automations:
+            automation.build_condition()
+            fpath = generate_automation_graph(
+                automation,
+                dest=os.path.join(gen_path, f"automation_{automation.name}.pu")
+            )
+            pu_to_png_transformation(fpath, gen_path)
+
+        make_tarball(tarball_path, gen_path)
+        shutil.rmtree(gen_path)
+        print(f'Sending tarball {tarball_path}')
+        return FileResponse(tarball_path,
+                            filename=os.path.basename(tarball_path),
+                            media_type='application/x-tar')
+    except Exception as e:
+        print(e)
+        resp['status'] = 404
+        return resp
+        if os.path.exists(gen_path):
+            shutil.rmtree(gen_path)
+
+
 def run_interpreter(model_path: str):
     pid = subprocess.Popen(['smauto', 'interpret', model_path], close_fds=True)
     return pid
+
+
+def make_tarball(fout, source_dir):
+    with tarfile.open(fout, "w:gz") as tar:
+        tar.add(source_dir, arcname=os.path.basename(source_dir))
